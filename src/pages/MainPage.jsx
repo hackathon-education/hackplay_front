0d2b4aca916ed77ab938fc31c1d356a5bc3676ce
dbstj589 <lysfox8@gmail.com>
2025-08-02 23:46:11 +0900

    refactor : mockup 이미지 변경

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/MainPage.css';
-import Mockup from '../assets/mockup.png'
+import Mockup from '../assets/mockup.jpg'
 
 function MainPage() {
     return (
