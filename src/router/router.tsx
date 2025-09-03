6c99618ec1f78aad41c94e26ef16bc60dc85aaaf
sy <ssyeo_n@naver.com>
2025-09-03 15:07:03 +0900

    ♻️ refactor: App 컴포넌트에서 라우팅 로직 분리 및 Router 컴포넌트 추가

---
+++ src/router/router.tsx
@@ -0,0 +1,39 @@
+import { Route, Routes } from 'react-router-dom';
+
+import BasicLearningPage from '../pages/BasicLearningPage';
+import CoursesPage from '../pages/CoursesPage';
+import LectureDetailPage from '../pages/LectureDetailPage';
+import LectureListPage from '../pages/LectureListPage';
+import LectureMainPage from '../pages/LectureMainPage';
+import LoginPage from '../pages/LoginPage';
+import MainPage from '../pages/MainPage';
+import ProjectsPage from '../pages/ProjectsPage';
+import SignupPage from '../pages/SignupPage';
+
+const Router = () => {
+  return (
+    <Routes>
+      {/* 메인 */}
+      <Route path="/" element={<MainPage />} />
+
+      {/* Auth */}
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/signin" element={<LoginPage />} />
+
+      {/* 단계별 학습 */}
+      <Route path="/courses" element={<CoursesPage />} />
+      <Route path="/courses/:job/:level" element={<LectureListPage />} />
+      <Route path="/courses/:job/:level/:lectureId" element={<LectureMainPage />} />
+      <Route path="/courses/:job/:level/:lectureId/detail" element={<LectureDetailPage />} />
+
+      {/* 기초 학습 */}
+      <Route path="/learning/basic" element={<BasicLearningPage />} />
+      <Route path="/learning/basic/:lectureId" element={<LectureDetailPage />} />
+
+      {/* 팀 프로젝트 */}
+      <Route path="/projects" element={<ProjectsPage />} />
+    </Routes>
+  );
+}
+
+export default Router;
