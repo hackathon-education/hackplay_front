610b38c9b2640204a20e659e86c3382da32f4bb0
sy <ssyeo_n@naver.com>
2025-09-03 16:12:03 +0900

    ✨ feat: 라우팅 상수 추가 및 Router 컴포넌트에서 상수 사용

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { ROUTES } from '../constants/routes';
 import BasicLearningPage from '../pages/BasicLearningPage';
 import CoursesPage from '../pages/CoursesPage';
 import LectureDetailPage from '../pages/LectureDetailPage';
@@ -14,26 +15,35 @@ const Router = () => {
   return (
     <Routes>
       {/* 메인 */}
-      <Route path="/" element={<MainPage />} />
+      <Route path={ROUTES.MAIN} element={<MainPage />} />
 
       {/* Auth */}
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/signin" element={<LoginPage />} />
+      <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+      <Route path={ROUTES.SIGNIN} element={<LoginPage />} />
 
       {/* 단계별 학습 */}
-      <Route path="/courses" element={<CoursesPage />} />
-      <Route path="/courses/:job/:level" element={<LectureListPage />} />
-      <Route path="/courses/:job/:level/:lectureId" element={<LectureMainPage />} />
-      <Route path="/courses/:job/:level/:lectureId/detail" element={<LectureDetailPage />} />
+      <Route path={ROUTES.COURSES.ROOT} element={<CoursesPage />} />
+      <Route path={ROUTES.COURSES.LECTURE_LIST(':job', ':level')} element={<LectureListPage />} />
+      <Route
+        path={ROUTES.COURSES.LECTURE_MAIN(':job', ':level', ':lectureId')}
+        element={<LectureMainPage />}
+      />
+      <Route
+        path={ROUTES.COURSES.LECTURE_DETAIL(':job', ':level', ':lectureId')}
+        element={<LectureDetailPage />}
+      />
 
       {/* 기초 학습 */}
-      <Route path="/learning/basic" element={<BasicLearningPage />} />
-      <Route path="/learning/basic/:lectureId" element={<LectureDetailPage />} />
+      <Route path={ROUTES.BASIC_LEARNING.ROOT} element={<BasicLearningPage />} />
+      <Route
+        path={ROUTES.BASIC_LEARNING.LECTURE_DETAIL(':lectureId')}
+        element={<LectureDetailPage />}
+      />
 
       {/* 팀 프로젝트 */}
-      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
     </Routes>
   );
-}
+};
 
 export default Router;
