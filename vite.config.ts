ea0e7e1bb50e1043cc416a4dc74bfc631684c64c
sy <ssyeo_n@naver.com>
2025-09-03 23:37:36 +0900

    🔧 chore: Path Alias 설정
    - @types/node 및 undici-types 추가
    - tsconfig.json 경로 설정 추가
    - vite.config.ts에 src 폴더를 @로 지정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5678,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
