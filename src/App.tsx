9ed62201a3bc140b2da06fb58e9997bbff4f55b3
sy <ssyeo_n@naver.com>
2025-12-18 04:47:54 +0900

    ✨ feat: 컴포넌트 마운트 시 hydrate()를 호출해 저장된 로그인 상태 복원 (#38)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Toaster } from 'sonner';
 
 import ScrollToTop from '@/components/ScrollToTop';
+import { useAuthStore } from '@/store/authStore';
 
 import Router from './router/router';
 
 const App = () => {
+  const hydrate = useAuthStore((state) => state.hydrate);
+
+  useEffect(() => {
+    hydrate();
+  }, [hydrate]);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
