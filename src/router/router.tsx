51f6f75c564928e2f77a7ca3b31a343fb59b86bd
sy <ssyeo_n@naver.com>
2025-12-18 01:51:01 +0900

    ✨ feat: 에러 페이지 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 
 import ProtectedRoute from '@/components/ProtectedRoute';
 import PublicRoute from '@/components/PublicRoute';
+import Layout from '@/components/Layout';
 import { ROUTES } from '@/constants/routes';
 import BasicLearningPage from '@/pages/BasicLearningPage';
 import CodeEditorPage from '@/pages/CodeEditorPage';
 import CoursesPage from '@/pages/CoursesPage';
+import ErrorPage from '@/pages/ErrorPage';
 import LectureDetailPage from '@/pages/LectureDetailPage';
 import LectureListPage from '@/pages/LectureListPage';
 import LectureMainPage from '@/pages/LectureMainPage';
@@ -17,41 +19,46 @@ import SignupPage from '@/pages/SignupPage';
 const Router = () => {
   return (
     <Routes>
-      {/* 메인 */}
-      <Route path={ROUTES.MAIN} element={<MainPage />} />
+      {/* 헤더/푸터가 있는 페이지들 */}
+      <Route element={<Layout />}>
+        <Route path={ROUTES.MAIN} element={<MainPage />} />
 
-      {/* Auth */}
-      <Route element={<PublicRoute />}>
-        <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
-        <Route path={ROUTES.SIGNIN} element={<LoginPage />} />
+        {/* Auth */}
+        <Route element={<PublicRoute />}>
+          <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+          <Route path={ROUTES.SIGNIN} element={<LoginPage />} />
+        </Route>
+
+        {/* 단계별 학습 */}
+        <Route path={ROUTES.COURSES.ROOT} element={<CoursesPage />} />
+        <Route path={ROUTES.COURSES.LECTURE_LIST(':job', ':level')} element={<LectureListPage />} />
+        <Route
+          path={ROUTES.COURSES.LECTURE_MAIN(':job', ':level', ':lectureId')}
+          element={<LectureMainPage />}
+        />
+        <Route
+          path={ROUTES.COURSES.LECTURE_DETAIL(':job', ':level', ':lectureId')}
+          element={<LectureDetailPage />}
+        />
+
+        {/* 코드 에디터 */}
+        <Route element={<ProtectedRoute />}>
+          <Route path={ROUTES.WORKSPACE(':lectureId')} element={<CodeEditorPage />} />
+        </Route>
+
+        {/* 기초 학습 */}
+        <Route path={ROUTES.BASIC_LEARNING.ROOT} element={<BasicLearningPage />} />
+        <Route
+          path={ROUTES.BASIC_LEARNING.LECTURE_DETAIL(':lectureId')}
+          element={<LectureDetailPage />}
+        />
+
+        {/* 팀 프로젝트 */}
+        <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
       </Route>
 
-      {/* 단계별 학습 */}
-      <Route path={ROUTES.COURSES.ROOT} element={<CoursesPage />} />
-      <Route path={ROUTES.COURSES.LECTURE_LIST(':job', ':level')} element={<LectureListPage />} />
-      <Route
-        path={ROUTES.COURSES.LECTURE_MAIN(':job', ':level', ':lectureId')}
-        element={<LectureMainPage />}
-      />
-      <Route
-        path={ROUTES.COURSES.LECTURE_DETAIL(':job', ':level', ':lectureId')}
-        element={<LectureDetailPage />}
-      />
-
-      {/* 코드 에디터 */}
-      <Route element={<ProtectedRoute />}>
-        <Route path={ROUTES.WORKSPACE(':lectureId')} element={<CodeEditorPage />} />
-      </Route>
-
-      {/* 기초 학습 */}
-      <Route path={ROUTES.BASIC_LEARNING.ROOT} element={<BasicLearningPage />} />
-      <Route
-        path={ROUTES.BASIC_LEARNING.LECTURE_DETAIL(':lectureId')}
-        element={<LectureDetailPage />}
-      />
-
-      {/* 팀 프로젝트 */}
-      <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
+      {/* 헤더/푸터가 없는 에러 페이지 */}
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 };
