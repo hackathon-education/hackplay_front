64f80ed67e94eed0dfdb53d584b198614e3738ad
sy <ssyeo_n@naver.com>
2026-01-30 03:03:30 +0900

    🔧 chore: SVG 컴포넌트 타입 선언 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 interface ImportMetaEnv {
   readonly VITE_SERVER_BASEURL: string;
+}
+
+declare module '*.svg?react' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default src;
 }
