f1206d0db5f232701abfc766e38c57419b509d3d
sy <ssyeo_n@naver.com>
2026-02-02 02:51:18 +0900

    💄 design: 헤더 레이아웃 구성 및 MainLayout에 헤더 높이만큼 패딩 추가 (#46)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,20 +8,25 @@ import { ROUTES } from '@/constants/routes';
 import HeaderTabs from './HeaderTabs';
 
 const Header = () => {
+  const headerClass = 'flex fixed w-full h-24 items-center justify-between px-5';
   const authClass =
     'flex w-20 h-9.5 items-center justify-center rounded-lg border shadow-1 text-sm font-medium';
 
   return (
-    <header>
-      <NavLink
-        to={ROUTES.MAIN}
-        className="flex w-[181px] h-14 items-center justify-center bg-logo-bg border border-logo-border rounded-20 shadow-1"
-      >
-        <LogoPrimary />
-      </NavLink>
-      <HeaderTabs variant="desktop" />
-      <HeaderTabs variant="mobile" />
-      <div className="flex gap-2.5">
+    <header className={headerClass}>
+      <div className="flex-1 flex justify-start">
+        <NavLink
+          to={ROUTES.MAIN}
+          className="flex w-[181px] h-14 items-center justify-center bg-logo-bg border border-logo-border rounded-20 shadow-1"
+        >
+          <LogoPrimary />
+        </NavLink>
+      </div>
+      <div className="flex-1 flex justify-center">
+        <HeaderTabs variant="desktop" />
+      </div>
+      {/* <HeaderTabs variant="mobile" /> - TODO: 모바일 메뉴 버튼 구현 */}
+      <div className="flex-1 flex justify-end gap-2.5">
         <Link
           to={ROUTES.SIGNIN}
           className={`${authClass} border-link-btn-default-border bg-link-btn-default-bg text-link-btn-default-text`}
@@ -35,8 +40,8 @@ const Header = () => {
           회원가입
         </Link>
       </div>
-      <FilePen strokeWidth={1.5} size={19} absoluteStrokeWidth={true} />
-      <img src={ProfileDefaultImg} alt="HACKPLAY" className="w-14" /> {/* TODO: API 연동 */}
+      {/* <FilePen strokeWidth={1.5} size={19} absoluteStrokeWidth={true} /> - TODO: 로그인 상태 분기 */}
+      {/* <img src={ProfileDefaultImg} alt="HACKPLAY" className="w-14" /> - TODO: 로그인 상태 분기, API 연동 */}
     </header>
   );
 };
