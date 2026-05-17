5c641ea4a819eb26578ebafb9b4bd0029ed1b3df
sy <ssyeo_n@naver.com>
2026-05-17 21:53:39 +0900

    ✨ feat: 마이페이지 보호 라우트 추가 및 강의 목록 라우트 제거 (#59)

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -11,10 +11,10 @@ import CodeEditorPage from '@/pages/CodeEditorPage';
 import CoursesPage from '@/pages/CoursesPage';
 import ErrorPage from '@/pages/ErrorPage';
 import LectureDetailPage from '@/pages/LectureDetailPage';
-import LectureListPage from '@/pages/LectureListPage';
 import LectureMainPage from '@/pages/LectureMainPage';
 import LoginPage from '@/pages/LoginPage';
 import MainPage from '@/pages/MainPage';
+import MyPage from '@/pages/MyPage';
 import ProjectsPage from '@/pages/ProjectsPage';
 import SignupPage from '@/pages/SignupPage';
 
@@ -42,10 +42,6 @@ const router = createBrowserRouter([
             element: <CoursesPage />,
           },
           {
-            path: ROUTES.COURSES.LECTURE_LIST(':job', ':level'),
-            element: <LectureListPage />,
-          },
-          {
             path: ROUTES.COURSES.LECTURE_MAIN(':job', ':level', ':lectureId'),
             element: <LectureMainPage />,
           },
@@ -59,6 +55,10 @@ const router = createBrowserRouter([
               {
                 path: ROUTES.WORKSPACE(':lectureId'),
                 element: <CodeEditorPage />,
+              },
+              {
+                path: ROUTES.MYPAGE,
+                element: <MyPage />,
               },
             ],
           },
